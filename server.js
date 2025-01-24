f0ebab8fc2041a336086d1f0ef1232fc121f0860
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 03:50:08 +0500

    code is added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ connectDB();
 console.log(process.env.PORT);
 
 //listen on port 4000:
-app.listen(4000, () => {
+app.listen(5000, () => {
     console.log(`Server is running on port: ${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
 })
 
