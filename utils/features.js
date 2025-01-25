5b24f658c2fd841334105600b2d8a0587565078b
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-26 03:59:15 +0500

    code is added

--- utils/features.js
+++ utils/features.js
@@ -13,6 +13,7 @@ export const sendCookie = (user, res, statusCode = 200, message) => {
         // by default lax hota hai cookie islye postman mai work krta hai but m chahti o jb front end 
         // ko backend sy attack krna ho tb sameSite: none ho and secure: true ho na k lax.. 
         // so do this:
+        // iss sy ye hoga k wo postman m b work kryga or after integration b...
         sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
         secure: process.env.NODE_ENV === "Development" ? false : true,
     })
