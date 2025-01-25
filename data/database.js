92275c1a751aaf6bdd736b1c00435222bfefe40c
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-26 00:44:34 +0500

    code is added

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: 'Todo',
-        writeConcern: { w: "majority" }
-    }).then(() => console.log("Database connected"))
+        // writeConcern: { w: "majority" }
+    }).then((c) => console.log(`Database connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 }
