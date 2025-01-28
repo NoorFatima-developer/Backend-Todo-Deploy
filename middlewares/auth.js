ecd910f891cfa312ff8d7c52a6d05bb134b4c522
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-28 16:00:23 +0500

    code is added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,11 +12,28 @@ export const isAuthenticated = async(req, res, next) => {
         })
     }
     
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+    console.log("Decoded token:", decoded);
+
 
     // jb hum simply cookie token sy data id mai bejty thy tu we save in a user
     // yahan pr we will save in a "req.user"
     req.user = await User.findById(decoded._id);
+    if (!req.user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
     // and isk bd next ko call krdea,,,and next kea hai wo m routes mai user.js mai dekh skti o..
     next();
-}
+    } catch (error) {
+        console.error("Error during authentication:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
+
+};
