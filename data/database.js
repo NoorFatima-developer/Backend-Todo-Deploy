d88789f2d7e90145b1fa18c1b06d699c4989dbdb
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-28 19:40:01 +0500

    code is added

--- data/database.js
+++ data/database.js
@@ -5,11 +5,11 @@ import mongoose from "mongoose";
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: 'Todo',
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        connectTimeoutMS: 60000,
-        socketTimeoutMS: 60000,
-        writeConcern: { w: "majority" }
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        // connectTimeoutMS: 60000,
+        // socketTimeoutMS: 60000,
+        // writeConcern: { w: "majority" }
         // cluster ka url get krny klye meny ye use kea hai: ${c.connection.host}
     }).then((c) => console.log(`Database connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
