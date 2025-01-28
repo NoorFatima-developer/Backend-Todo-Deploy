41043032707d2a062ca653102efb280842e7ddf5
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-28 18:24:31 +0500

    code is added

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/new", register);
 router.post("/login", login);
 // router.get("/me" , getmyProfileWithsimplycookiedecodedtoken);
-router.get("/isauth" , isAuthenticated, getmyProfilebyAuthenticatedMiddleware);
+router.get("/me" , isAuthenticated, getmyProfilebyAuthenticatedMiddleware);
 router.get("/logout", logout);
 // ye hum tb use krty hain jb hum admin dashboard bana rye o or hmain sary users ka data show krwana ho tb..
 // router.get("/all", getAllUsers);
